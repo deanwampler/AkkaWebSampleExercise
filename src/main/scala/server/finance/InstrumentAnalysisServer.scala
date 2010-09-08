6d6b34618c3164fec1999ff0d119c0cd910af396
Dean Wampler <dean@deanwampler.com>
2010-09-08 15:43:01 -0500

    bug fix

--- src/main/scala/server/finance/InstrumentAnalysisServer.scala
+++ src/main/scala/server/finance/InstrumentAnalysisServer.scala
@@ -77,7 +77,7 @@ class InstrumentAnalysisServerHelper(dataStorageServer: => ActorRef) {
       case None => 
         Pair("warning", "Nothing returned for query (start, end) = (" + start + ", " + end + ")")
       case Some(result) => 
-        formatPriceResults(filter(result, instruments), instruments, statistics, startMillis, endMillis)
+        formatPriceResults(filter(result), instruments, statistics, startMillis, endMillis)
     }
   }
   
@@ -85,21 +85,7 @@ class InstrumentAnalysisServerHelper(dataStorageServer: => ActorRef) {
    * A "hook" method that could be used to filter by instrument (and maybe statistics) criteria. 
    * However, in general, it would be better to filter in the DB query itself!
    */
-  protected def filter(json: JValue, instruments: List[Instrument]): JValue = {
-    val names = Instrument.toSymbolNames(instruments)
-    // log.error ("json: "+json.toString)
-    // log.error ("json.values: "+json.values.toString)
-    // log.error ("symbol: "+(json \\ "symbol").toString)
-    json match {
-      case JArray(list) => list filter { element => 
-        (element \\ "symbol") match {
-          case JField("symbol", x) if (names.exists(x == JString(_))) => true
-          case _ => false
-        }
-      }
-      case _ => true
-    }
-  }
+  protected def filter(json: JValue): JValue = json
   
   // Public visibility, for testing purposes.
   def formatPriceResults(
